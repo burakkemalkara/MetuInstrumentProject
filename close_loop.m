d1e94c92fe451868e3d57334c9bc911cf870339d
Burak Kemal Kara <56311262+burakkemalkara@users.noreply.github.com>
2020-06-24 11:07:27 +0300

    Tranfer function and calculations are completed

--- close_loop.m
+++ close_loop.m
@@ -6,6 +6,17 @@ n=2;
 v_i=36;
 s = tf('s');
 H= [(n*v_i)/(L*C)]*[(s*C*r_c+1)/(s^2+s*((r_c/L)+(1/(R*C)))+1/(L*C))];
+R1=1000;
+R2=10770;
+R3=130;
+R4=1500;
+C1=2e-9;
+C2=0.26e-9;
+C3=22e-9;
+%Compensator Bode plot
+G_div=1/3;
+G=-R4/((R4+R1)*R3*C2);
+H_c=tf(G_div*G*[1 (1/(R2*C1))+(1/(R1*C3)) 1/(R1*R2*C1*C3)],[1 (1/(R2*C2))+(1/(R3*C3)) 1/(R2*R3*C2*C3) 0]);
 
 opts = bodeoptions;
 opts.Title.FontSize = 14;
@@ -13,6 +24,7 @@ opts.xlabel.FontSize=14;
 opts.ylabel.FontSize=14;
 opts.FreqUnits = 'Hz';
 figure (1)
-bode(H,opts)
+
+bode(H*H_c,opts)
 grid on
 set(findall(gcf,'type','line'),'linewidth',1.25)
